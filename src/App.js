dac81527607d76e2f3b4b186d955f7d3e7f7537b
Axilotl <axilotl17@gmail.com>
2023-02-28 15:55:52 -0800

    embed test

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import Runs from "./screens/runs";
 import Links from "./screens/links";
 import About from "./screens/about";
 
+
+function mapTest() {
+  return (
+    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=180yszlUoz3aIYtUxEGN-bm9JRybFSoY&ehbc=2E312F" width="640" height="480"></iframe>
+  );
+}
+
 export default function App() {
   return (
     <div className="w-full h-full flex">
+      <mapTest />
       <div>Hello</div>
     </div>
   );
